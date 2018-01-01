5777fc17b42c5e36a998f630e7ca3cb7eef89094
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2018-01-01 19:20:38 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,17 @@
+
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Response.new
+    if req.path.match("/items")
+      name = req.path.split("/items/").last
+      resp.write "#{name}\n"
+    else
+      resp.write("File not found.\n")
+      resp.status = 404
+    end
+    resp.finish
+  end
+
+end

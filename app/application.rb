8228a1bffa58e25a18c81b1de77826cc151641c0
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2018-01-01 19:25:20 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,13 +2,13 @@
 class Application
 
   @@items = []
-  
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new
     if req.path == "/items"
       name = req.path.split("/items/").last
-      resp.write "#{name}\n"
+      resp.write "#{@@items[name].price}\n"
     else
       resp.write("File not found.\n")
       resp.status = 404
